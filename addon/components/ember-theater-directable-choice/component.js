d45b7792b38e70d4c056a1a277a10a16deb05fe5
null null null <null@revolution.cheap>
2015-08-16 20:17:52 -0700

    make choice intl

--- addon/components/ember-theater-directable-choice/component.js
+++ addon/components/ember-theater-directable-choice/component.js
@@ -7,22 +7,49 @@ import WindowResizeMixin from '../../mixins/window-resize';
 const {
   Component,
   computed,
+  inject,
   on
 } = Ember;
 
 export default Component.extend(DirectableComponentMixin, PerfectScrollbarMixin, WindowResizeMixin, {
   classNameBindings: ['line.class'],
   classNames: ['ember-theater-stage__choice'],
+  intlWrapper: inject.service(),
   layout: layout,
 
-  choices: computed('line.choices', function() {
-    const choices = this.get('line.choices');
-    const keys = Object.keys(choices);
+  choices: computed('line.choices', 'line.intl.choices', {
+    get() {
+      const intlChoices = this.get('line.intl.choices');
+      const choices = intlChoices ? intlChoices : this.get('line.choices');
+      const keys = Object.keys(choices);
 
-    return keys.map((key) => {
-      return { path: key, text: choices[key] };
-    });
-  }),
+      return keys.map((key) => {
+        let text;
+
+        if (intlChoices) {
+          const intlKey = choices[key].id ? choices[key].id : choices[key];
+          text = this.get('intlWrapper').formatMessage(intlKey, choices[key].options);
+        } else {
+          text = choices[key];
+        }
+
+        return { path: key, text: text };
+      });
+    }
+  }).readOnly(),
+
+  header: computed('line.header', 'line.intl.header', {
+    get() {
+      const intlHeader = this.get('line.intl.header');
+
+      if (intlHeader) {
+        const intlKey = intlHeader.id ? intlHeader.id : intlHeader;
+        return this.get('intlWrapper').formatMessage(intlKey, intlHeader.options);
+      }
+
+      return this.get('line.header');
+    }
+  }).readOnly(),
 
   actions: {
     choose(path) {
--- tests/dummy/app/ember-theater-scenes/1.js
+++ tests/dummy/app/ember-theater-scenes/1.js
@@ -2,6 +2,8 @@ import Scene from 'ember-theater/models/ember-theater-scene';
 
 export default Scene.extend({
   script: async function() {
+    const transChoice = await this.choice({ intl: { header: '1.choice1.header', choices: { A: { id: '1.choice1.A', options: { opt1: 'OOO' } }, B: '1.choice1.B' } } });
+    alert(transChoice);
     await this.dialogue({ character: 'steven', intl: { id: '1.firstTest', options: { val1: 'my friend' } } });
     this.dialogue({ character: 'steven', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec purus diam. Duis condimentum sapien quis nisl dictum, et volutpat arcu mollis. Fusce nec nisl non eros lobortis fringilla. Aliquam ullamcorper eros efficitur rhoncus luctus. Phasellus odio velit, finibus a pharetra vel, accumsan id urna. Sed vitae egestas nisi. Nulla eu urna sed nulla pulvinar volutpat at quis nunc. Morbi vel purus est. Curabitur rutrum mauris a cursus tempus. Suspendisse leo eros, scelerisque porttitor leo a, vehicula facilisis quam. Mauris blandit tempor dolor a interdum. Curabitur rutrum arcu ac mi pharetra pellentesque. Nullam sodales gravida quam tempus lobortis. Mauris consequat mauris aliquam justo posuere convallis. Aliquam mauris est, lacinia condimentum tempus sit amet, cursus id lorem.' });
     await this.backdrop({ id: 'beach', options: { duration: 1000 } });
