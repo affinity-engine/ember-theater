5bccdd47ed7fbc2ad1ff13b5063ef64d997caec3
null null null <null@revolution.cheap>
2015-09-04 18:18:47 -0700

    fix main generator

--- blueprints/ember-theater/index.js
+++ blueprints/ember-theater/index.js
@@ -3,7 +3,10 @@ var EOL = require('os').EOL;
 module.exports = {
   description: 'Generates files for ember theater',
 
+  normalizeEntityName: function() {},
+
   afterInstall: function() {
+    var _this = this;
     var packages = [{
       name: 'babel-polyfill',
       target: '~0.0.1'
@@ -12,12 +15,14 @@ module.exports = {
       target: '~1.3.9'
     }]; 
     
-    this.addBowerPackagesToProject(packages);
-    this.addAddonToProject({ name: 'ember-intl', target: '2.0.0-beta.22' });
-    this.addAddonToProject({ name: 'ember-keyboard-service', target: '0.3.0' });
-
-    this.insertIntoFile('ember-cli-build.js',
-                        'babel: { includePolyfill: true, stage: 2 }',
-                        { after: 'var app = new EmberApp(defaults, {' + EOL });
+    return this.addBowerPackagesToProject(packages).then(function() {
+      return _this.addAddonToProject({ name: 'ember-intl', target: '2.0.0-beta.22' });
+    }).then(function() {
+      return _this.addAddonToProject({ name: 'ember-keyboard-service', target: '0.3.0' });
+    }).then(function() {
+      return _this.insertIntoFile('ember-cli-build.js',
+        '    babel: { includePolyfill: true, stage: 2 }',
+        { after: 'var app = new EmberApp(defaults, {' + EOL });
+    });
   }
 };
