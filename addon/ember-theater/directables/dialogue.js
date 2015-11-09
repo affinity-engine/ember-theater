8b027fe38a68045cd4b9d9cef62569c074f917b3
null null null <null@revolution.cheap>
2015-11-08 21:56:27 -0800

    more flexible dialogue arguments

--- addon/ember-theater/directables/dialogue.js
+++ addon/ember-theater/directables/dialogue.js
@@ -1,13 +1,38 @@
 import Ember from 'ember';
 import { Directable } from 'ember-theater';
 
-const { setProperties } = Ember;
+const {
+  get,
+  isPresent,
+  setProperties,
+  typeOf
+} = Ember;
 
 export default Directable.extend({
   componentType: 'ember-theater/director/dialogue',
   layer: 'theater.text.dialogue',
 
-  parseArgs(character, text, options) {
+  parseArgs(characterOrText, textOrOptions, optionsOnly) {
+    let character, text, options;
+
+    // 10 megabucks to anyone who can devise a cleaner approach to this
+    // Note that character and text can come as either strings or objects
+    if (isPresent(optionsOnly)) {
+      character = characterOrText;
+      text = textOrOptions;
+      options = optionsOnly;
+    } else if (isPresent(textOrOptions)) {
+      if (typeOf(textOrOptions) === 'string' || (typeOf(textOrOptions) === 'object' && isPresent(get(textOrOptions, 'id')))) {
+        character = characterOrText;
+        text = textOrOptions;
+      } else {
+        text = characterOrText;
+        options = textOrOptions;
+      }
+    } else {
+      text = characterOrText;
+    }
+
     setProperties(this, {
       character,
       text,
