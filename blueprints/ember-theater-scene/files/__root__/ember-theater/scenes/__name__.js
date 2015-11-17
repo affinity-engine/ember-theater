edae982fa81bf4b6745f46d0392562fd8a8d45ab
null null null <null@revolution.cheap>
2015-11-16 21:36:01 -0800

    update scene blueprint to reflect scene name

--- blueprints/ember-theater-scene/files/__root__/ember-theater/scenes/__name__.js
+++ blueprints/ember-theater-scene/files/__root__/ember-theater/scenes/__name__.js
@@ -1,6 +1,8 @@
 import { Scene } from 'ember-theater';
 
 export default Scene.extend({
+  name: '<%= capitalizedModuleName %>',
+
   script: async function(director) {
   }
 });
--- blueprints/ember-theater-scene/index.js
+++ blueprints/ember-theater-scene/index.js
@@ -1,3 +1,11 @@
 module.exports = {
-  description: 'Generates a scene for ember-theater.'
+  description: 'Generates a scene for ember-theater.',
+
+  locals: function(options) {
+    return {
+      capitalizedModuleName: options.entity.name.split(/-|_/).map(function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(' ')
+    }
+  }
 };
