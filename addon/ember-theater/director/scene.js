c560b5987fc98397eb23dfd7576e589e3f6f1d9a
null null null <null@ember.theater>
2016-03-19 11:03:23 -0700

    cleanup Scene

--- addon/ember-theater/director/scene.js
+++ addon/ember-theater/director/scene.js
@@ -1,12 +1,3 @@
 import Ember from 'ember';
-import multitonService from 'ember-theater/macros/ember-theater/multiton-service';
 
-const { set } = Ember;
-
-export default Ember.Object.extend({
-  director: multitonService('ember-theater/director/director', 'theaterId'),
-
-  abort() {
-    set(this, 'isAborted', true);
-  }
-});
+export default Ember.Object.extend();
