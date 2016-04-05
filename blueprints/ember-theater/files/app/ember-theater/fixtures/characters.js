4bf0c2c0a0889184c7548542f317103f94f3cb61
null null null <null@ember.theater>
2016-04-05 08:56:49 -0700

    update blueprints

--- blueprints/ember-theater/files/app/ember-theater/fixtures/characters.js
+++ blueprints/ember-theater/files/app/ember-theater/fixtures/characters.js
@@ -2,15 +2,21 @@
   @property id {String} The id by which you'll reference this character. Not necessarily the same as the character's name.
   @property name {String} The character's name. It is to be used wherever the character is referenced to the user.
   @property height {Number} The character's height. What percentage of the screen height is fills.
-  @property defaultExpressionId {String} The id of the expression that this character will use by default.
+  @property defaultExpression {String} The id of the expression that this character will use by default.
+  @property expressions {Object} Key/value aliases for the character's expressions.
 
   Example:
 
   {
-    id: 'steven',
-    name: 'Steven',
+    id: 'bitsy',
+    name: 'Bitsy',
     height: 60,
-    defaultExpressionId: 'steven'
+    defaultExpression: 'neutral',
+    expressions: {
+      neutral: 'bitsy-neutral',
+      happy: 'bitsy-happy',
+      angry: 'bitsy-angry'
+    }
   }
 */
 
--- blueprints/ember-theater/files/app/ember-theater/fixtures/expressions.js
+++ blueprints/ember-theater/files/app/ember-theater/fixtures/expressions.js
@@ -2,13 +2,13 @@
   @property id {String} The id by which you'll reference this expression.
   @property caption {String} The alt text that will appear for this image. Primarily used for accessability.
   @property src {String} The path to the image, relative to the `public` directory.
-  
+
   Example:
 
   {
-    id: 'steven--jumping',
-    caption: 'Steven Universe jumping',
-    src: 'images/expressions/steven--jumping.png'
+    id: 'bitsy-neutral',
+    caption: 'Bitsy with a neutral expression,
+    src: 'images/expressions/bitsy-neutral.png'
   }
 */
 
