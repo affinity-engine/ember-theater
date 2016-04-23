68ef0435106e58d1f35d9f716185a4417ce01a4d
null null null <null@ember.theater>
2016-04-23 11:16:20 -0700

    fix bugs

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ module.exports = {
     app.import(path.join(app.bowerDirectory, 'PreloadJS/lib/preloadjs-NEXT.combined.js'));
     app.import(path.join(app.bowerDirectory, 'SoundJS/lib/soundjs-NEXT.combined.js'));
     app.import(path.join(app.bowerDirectory, 'progressbar.js/dist/progressbar.min.js'));
-    app.import('vendor/perfect-scrollbar/dist/js/min/perfect-scrollbar.min.js');
+    app.import('vendor/perfect-scrollbar/dist/js/perfect-scrollbar.min.js');
     app.import('vendor/perfect-scrollbar/dist/css/perfect-scrollbar.min.css');
     app.import('vendor/velocity-animate/velocity.js');
     app.import('vendor/velocity-animate/velocity.ui.js');
