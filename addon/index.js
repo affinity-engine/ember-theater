64a8ba04f86a72c919b47d6c7d33e7e1332af776
null null null <null@ember.theater>
2016-05-19 09:18:58 -0700

    destructure configurable

--- addon/index.js
+++ addon/index.js
@@ -1,7 +1,7 @@
 import ConfigurableMixin from 'ember-theater/mixins/configurable';
 import MultitonIdsMixin from 'ember-theater/mixins/multiton-ids';
 
-import configurable from 'ember-theater/macros/ember-theater/configurable';
+import configurable, { deepConfigurable, deepArrayConfigurable } from 'ember-theater/macros/ember-theater/configurable';
 
 import animate from 'ember-theater/utils/ember-theater/animate';
 import deepMerge from 'ember-theater/utils/ember-theater/deep-merge';
@@ -11,6 +11,8 @@ export {
   ConfigurableMixin,
   MultitonIdsMixin,
   configurable,
+  deepConfigurable,
+  deepArrayConfigurable,
   animate,
   deepMerge,
   nativeCopy
