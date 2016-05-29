fde0625b74c7a668a9f597bf69ceb52c933e625a
null null null <null@ember.theater>
2016-05-28 17:07:31 -0700

    add all global config

--- addon/ember-theater/configs/ember-theater.js
+++ addon/ember-theater/configs/ember-theater.js
@@ -1,5 +1,27 @@
 export default {
   priority: 0,
+  globals: {
+    classNames: ['et-paper', 'et-block'],
+    transitionDuration: 200,
+    transition: {
+      duration: 250,
+      effect: { opacity: 1 }
+    },
+    transitionIn: {
+      duration: 250,
+      effect: { opacity: [1, 0] }
+    },
+    transitionOut: {
+      duration: 250,
+      effect: { opacity: 0 }
+    },
+    keys: {
+      accept: [' ', 'Enter'],
+      cancel: ['Escape'],
+      moveDown: ['ArrowDown', 's'],
+      moveUp: ['ArrowUp', 'w']
+    }
+  },
   saveStateManager: {
     maxStatePoints: false
   },
