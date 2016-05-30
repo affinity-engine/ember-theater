f658072b49b22702a468152578dbd5cd481172aa
null null null <null@ember.theater>
2016-05-30 11:10:10 -0700

    fix blueprint

--- blueprints/ember-theater/index.js
+++ blueprints/ember-theater/index.js
@@ -1,14 +1,10 @@
 module.exports = {
   description: 'Generates files for ember theater',
 
-  normalizeEntityName: function() {},
-
   afterInstall: function() {
     var _this = this;
 
-    return this.addBowerPackagesToProject(packages).then(function() {
-      return _this.addAddonToProject({ name: 'ember-i18n', target: '4.2.0' });
-    }).then(function() {
+    return this.addAddonToProject({ name: 'ember-i18n', target: '4.2.0' }).then(function() {
       return _this.addAddonToProject({ name: 'ember-moment', target: '6.1.0' });
     });
   }
